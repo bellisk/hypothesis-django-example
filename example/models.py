7178e6995ed99be49b31b9efac1f0ebdb59f35b9
Zarkonnen <david.stark@zarkonnen.com>
2015-05-29 23:13:21 +0200

    Added Portfolio example that does an incorrect median calculation.

--- example/models.py
+++ example/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 import datetime
 
-# Create your models here.
 class StockSubscription(models.Model):
     symbol = models.CharField(max_length=8)
     lastNotified = models.DateTimeField(auto_now_add=True)
     notificationsPerDay = models.IntegerField()
+    value = models.IntegerField()
 
     def __str__(self):
         return '"' + self.symbol + '" ' + str(self.notificationsPerDay) + ' per day'
@@ -14,15 +14,20 @@ class StockSubscription(models.Model):
     def next_notification_time(self):
         return self.lastNotified + datetime.timedelta(hours=24/self.notificationsPerDay)
 
-
-class RelatedSubscription(models.Model):
-    baseSubscription = models.ForeignKey(StockSubscription)
-    relatednessFactor = models.FloatField()
-
+class Portfolio(models.Model):
+    subscriptions = models.ManyToManyField(StockSubscription, related_name="portfolios")
+    
+    def median_value(self):
+        return median([sub.value for sub in self.subscriptions.all()])
+    
     def __str__(self):
-        return 'Stocks related to ' + str(self.baseSubscription) + \
-               ' with a relatedness factor of ' + str(self.relatednessFactor)
+        return "A portfolio of stock subscriptions."
 
-    @property
-    def next_notification_time(self):
-        return self.baseSubscription.next_notification_time
+def median(l):
+    if len(l) == 0:
+        return 0
+    l = sorted(l)
+    if len(l) % 2 == 0:
+        return (l[len(l) / 2] + l[len(l) / 2 + 1]) / 2
+    else:
+        return l[len(l) / 2]
