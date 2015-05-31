3769ce4f8915fb2d1398746fe51fe2eb3249f890
bellisk <daisywheel22@gmail.com>
2015-05-31 16:16:42 +0100

    Use integer strategy.

--- example/tests.py
+++ example/tests.py
@@ -1,10 +1,11 @@
 from hypothesis.extra.django import TestCase
 from hypothesis.extra.django.models import models
 from hypothesis import given
+from hypothesis.strategies import integers
 from mysite.example.models import StockSubscription
 
 
 class StockSubscriptionTestCase(TestCase):
-    @given(models(StockSubscription))
+    @given(models(StockSubscription, notificationsPerDay=integers()))
     def test_next_notification_after_last_notification(self, subscription):
         self.assertTrue(subscription.next_notification_time > subscription.lastNotified)
