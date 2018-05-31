910cf0d58d6e9fb6748a44c240a5f1ac86b2917f
Rae Knowler <rae.knowler@liip.ch>
2018-05-31 14:06:32 +0200

    add super simple example

---
+++ very_simple_example.py
@@ -0,0 +1,10 @@
+from hypothesis import given
+from hypothesis.strategies import integers, lists
+
+
+def mysort(unsorted_array):
+    return unsorted_array
+
+@given(lists(integers()))
+def test_mysort(unsorted_array):
+    assert(mysort(unsorted_array) == sorted(unsorted_array))
